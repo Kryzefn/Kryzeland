b0d4ae04a0f10a8d69c16a472d036f4b29ea6047
Glitch (mini-sticky-barge) <none>
2020-12-03 17:45:37 +0000

    🏢🚣 Checkpoint ./watch.json:25756333/210 ./server.js:25756333/429 ./index.js:25756333/476

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+
+//
+app.use(express.static('public'));
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+
+app.listen(process.env.PORT);
+
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`); 
+}, 280000);
+
+////////////////////BOT////////////////////
+
 /**
  * Module Imports
  */
